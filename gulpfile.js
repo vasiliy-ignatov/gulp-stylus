33e10defb4872726192a02d123d1915cfca5eceb
vasiliy-ignatov <inaburat@gmail.com>
2021-09-25 22:57:08 +0300

    fonts example

--- gulpfile.js
+++ gulpfile.js
@@ -8,16 +8,28 @@ const pug = require('gulp-pug')
 const concat = require('gulp-concat')
 const babel = require('gulp-babel')
 
-const PATHS = {
-	MODULES:'./src/modules',
-	VENDOR: './src/vendor',
-	IN: './src',
-	OUT: './htdocs'
+// const PATHS = {
+// 	MODULES:'./src/modules',
+// 	VENDOR: './src/vendor',
+// 	IN: './src',
+// 	OUT: './htdocs',
+// 	ASSETS: './src/assets'
+// }
+const config = {
+	in: {
+		src: './src',
+		assets: './src/assets',
+		modules: './src/modules'
+	},
+	out: {
+		htdocs: './htdocs',
+		assets: './htdocs/assets'
+	}
 }
 
 function clean () {
 	return del(
-		PATHS.OUT
+		config.out.htdocs
 	)
 }
 
@@ -29,66 +41,66 @@ function browserSync () {
 		}
 	})
 
-	watch(PATHS.OUT  + '/css/*.css').on('change', browsersync.reload)
+	watch(config.out.assets  + '/css/*.css').on('change', browsersync.reload)
 }
 
 function css() {
-	return src(PATHS.MODULES + '/*.styl')
+	return src(config.in.modules + '/*.styl')
 		.pipe(stylus())
 		.pipe(concat('main.css'))
-		.pipe(dest(PATHS.OUT + '/css'))
+		.pipe(dest(config.out.assets + '/css'))
 }
 
 
 function js() {
-	return src(PATHS.MODULES + '/**/*.js')
+	return src(config.in.src + '/**/*.js')
 		.pipe(concat('main.js'))
 		.pipe(babel({
 			presets: ['@babel/preset-env']
 		}))
-		.pipe(dest(PATHS.OUT + '/js'))
+		.pipe(dest(config.out.assets + '/js'))
 }
 
 function html() {
-	return src(PATHS.IN + '/*.pug')
+	return src(config.in.src + '/*.pug')
 		.pipe(pug({
 			pretty: true
 		}))
-		.pipe(dest(PATHS.OUT))
+		.pipe(dest(config.out.htdocs))
 }
 
 function jquery() {
 	return src('node_modules/jquery/dist/jquery.min.js')
-		.pipe(dest(PATHS.OUT + '/js'))
+		.pipe(dest(config.out.assets + '/js'))
 }
 
 function fonts() {
-	return src(PATHS.IN + '/fonts/*.*')
-		.pipe(dest(PATHS.OUT + '/fonts/'));
+	return src(config.in.assets + '/fonts/*.*')
+		.pipe(dest(config.out.assets + '/fonts/'));
 }
 
 function img() {
-	return src(PATHS.IN + '/img/**/*.*')
-		.pipe(dest(PATHS.OUT + '/img/'));
+	return src(config.in.assets + '/img/**/*.*')
+		.pipe(dest(config.out.assets + '/img/'));
 }
 
 function vendorJs() {
-	return src(PATHS.VENDOR + '/**/*.js')
+	return src(config.in.assets + '/**/*.js')
 		.pipe(concat('vendor.js'))
-		.pipe(dest(PATHS.OUT + '/js'))
+		.pipe(dest(config.out.assets + '/js'))
 }
 
 function vedorCss() {
-	return src([PATHS.VENDOR + '/css/normalize.css', PATHS.VENDOR + '/**/*.css'])
+	return src([config.in.assets + '/vendor/css/normalize.css', config.in.assets + '/vendor/**/*.css'])
 		.pipe(concat('vendor.css'))
-		.pipe(dest(PATHS.OUT + '/css'))
+		.pipe(dest(config.out.assets + '/css'))
 }
 
 
 function watchFiles() {
-	watch(PATHS.IN  + '/**/*.pug', html)
-	watch(PATHS.MODULES  + '/**/*.styl', css)
-	watch(PATHS.MODULES  + '/**/*.js', js)
+	watch(config.in.src  + '/**/*.pug', html)
+	watch(config.in.src  + '/**/*.styl', css)
+	watch(config.in.src  + '/**/*.js', js)
 }
 
 const vendor = series(vendorJs, vedorCss)
--- htdocs/js/main.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-var sum = function sum(a, b) {
-  return a + b;
-};
-
-console.log(sum(1, 2));
